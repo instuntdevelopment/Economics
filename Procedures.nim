8974eb3d68641aaec325dce071ba54fb741eec4e
instuntdevelopment <85498934+instuntdevelopment@users.noreply.github.com>
2023-12-17 20:00:36 +0100

    Update Procedures.nim

--- Procedures.nim
+++ Procedures.nim
@@ -1,4 +1,5 @@
 import std/random
+import tables
 randomize()
 
 type
@@ -12,10 +13,11 @@ type
     profession: string
 
 type
-  Datalist = array[3, string]
+  Namelist = array[3, string]
+  Professionlist = array[3, string]
 
-let names: Datalist = ["Arnold", "Matus", "Boris"]
-let professions: Datalist = ["robotnik", "ucitel", "predavac"]    
+let names: Namelist = ["Arnold", "Matus", "Boris"]
+let professions: Professionlist = ["robotnik", "ucitel", "predavac"]    
 
 iterator items(range: CustomRange): int =
   var i = range.low
@@ -27,15 +29,18 @@ iterator pairs(range: CustomRange): tuple[a: int, b: char] =
   for i in range:  # uses CustomRange.items
     yield (i, char(i + ord('a')))
 
-proc make_population(n: int) =
+proc make_population(n: int):OrderedTable[int, seq[string]]=
+  result = initOrderedTable[int, seq[string]]()
   for i, c in CustomRange(low: 1, high: n):
+    result[i] = @[]
     # Pick a number in 0..2.
     let num = rand(2)
     doAssert num in 0..2
-    echo names[num]
-
     let num2 = rand(2)
-    doAssert num2 in 0..2
-    echo professions[num2]  
+    doAssert num2 in 0..2 
 
-make_population(10)
+    result[i].add(names[num])
+    result[i].add(professions[num2])
+
+var population_list = make_population(10)
+echo population_list
